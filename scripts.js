760c3be08c514c92a6f8e505178634dcc49b4bef
Skryre <zbk1998@ymail.com>
2022-10-03 08:40:56 -0400

    Fixed the issue with game in the loop, however i dont think the code track the number of win the computer or i have

--- scripts.js
+++ scripts.js
@@ -1,21 +1,3 @@
-//the game
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-      return "Tie"
-      tieScore++;
-    }
-    else if (playerSelection === "rock" && computerSelection === "scissors" 
-    || playerSelection === "scissors" && computerSelection === "paper" 
-    || playerSelection === "paper" && computerSelection === "rock") {
-      return "Player won";
-      playerScore++;
-    }
-    else {
-      return "Machine won"
-      computerScore++;
-    }
-  }
-  
   //computer selection
   var computerRandom =  Math.random() * 3;
   console.log(computerRandom);
@@ -38,16 +20,33 @@ function playRound(playerSelection, computerSelection) {
   console.log("You played"+" "+playerSelection);
   
   //score tracking
-  let playerScore=0;
-  let computerScore=0;
-  let tieScore=0;
-  
+  let playerScore = 0;
+  let computerScore = 0;
+  let tieScore = 0;
+
+  //the game
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return "Tie";
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors" 
+    || playerSelection === "scissors" && computerSelection === "paper" 
+    || playerSelection === "paper" && computerSelection === "rock") {
+      playerScore = playerScore++;
+      return "Player won";
+    }
+    else {
+      computerScore = computerScore++;
+      return "Machine won";
+    }
+  }
   
   console.log(playRound(playerSelection, computerSelection));
   
-  //function of the Loop !!
+
   function game () {
   
+  //function of the Loop !!
   for (let i = 0; i < 4; i++) {
   
   //computer selection
@@ -69,22 +68,38 @@ function playRound(playerSelection, computerSelection) {
   
   //player selection
   const playerSelection = prompt("What do you choose between rock, paper and scissors?");
-  console.log("You played"+" "+playerSelection); 
+  console.log("You played"+" "+playerSelection);
+
+  //game function in the loop
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return "Tie";
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors" 
+    || playerSelection === "scissors" && computerSelection === "paper" 
+    || playerSelection === "paper" && computerSelection === "rock") {
+      playerScore = playerScore++;
+      return "Player won";
+    }
+    else {
+      computerScore = computerScore++;
+      return "Machine won";
+    }
+  }
+
+  console.log(playRound(playerSelection, computerSelection));
   }
   
-  
-    if (playerScore>computerScore){
-       return "You Win this game.";
-     }
-       if (playerScore==computerScore){
-         return "This game is a tie.";
-       }
-      else{
-       return "You Lose this game.";
-     };
-  
+  if (playerScore > computerScore){
+      return "You win this game.";
   }
-  
+  else{
+    return "You lose this game.";
+  }
+  }
+
   //play the Loop
   console.log(game());
   
+
+// TROUVER UNE FACONS DE FAIRE POUR SAUVER LE NOMBRE DE FOIS JOUÉ
