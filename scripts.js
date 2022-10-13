08cd1060050e03a6391b2c7dae6c75d1bbbf846c
Skryre <zbk1998@ymail.com>
2022-10-12 21:30:00 -0400

    Started working on the js part and building the logic with the player and computer selection

--- scripts.js
+++ scripts.js
@@ -1,103 +1,57 @@
-  var computerRandom =  Math.random() * 3;
-  console.log(computerRandom);
-  function getComputerChoice() {
-    if (computerRandom <= 1) {
-      return "rock"
-    }
-    else if (computerRandom <= 2) {
-      return "scissors"
-    }
-    else {
-      return "paper"
-    }
-  };
-  const computerSelection = getComputerChoice();
-  console.log("The Machine played"+" "+computerSelection);
-  
-  const playerSelection = prompt("What do you choose between rock, paper and scissors?");
-  console.log("You played"+" "+playerSelection);
-  
-  let playerScore = 0;
-  let computerScore = 0;
-  let tieScore = 0;
+const paperBtn = document.querySelector('#paperBtn');
+paperBtn.addEventListener('click', () => {
+  playerSelection = "paper";
+});
 
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-      return "Tie";
-    }
-    else if (playerSelection === "rock" && computerSelection === "scissors" 
-    || playerSelection === "scissors" && computerSelection === "paper" 
-    || playerSelection === "paper" && computerSelection === "rock") {
-      playerScore++;
-      return "Player won";
-    }
-    else {
-      computerScore++;
-      return "Machine won";
-    }
+const rockBtn = document.querySelector('#rockBtn');
+rockBtn.addEventListener('click', () => {
+  playerSelection = "rock";
+});
+
+const scissorsBtn = document.querySelector('#scissorsBtn');
+scissorsBtn.addEventListener('click', () => {
+   playerSelection = "scissors";
+});
+
+console.log("You played"+" "+playerSelection);
+
+
+var playerSelection = "";
+
+var computerRandom =  Math.random() * 3;
+function getComputerChoice() {
+  if (computerRandom <= 1) {
+    return "rock"
   }
-
-  console.log(playRound(playerSelection, computerSelection));
-
-  console.log("Player score"+" "+ playerScore)
-
-  console.log("Machine score"+" "+ computerScore)
-
-  function game () {
-  
-  for (let i = 0; i < 4; i++) {
-  
-  var computerRandom =  Math.random() * 3;
-  console.log(computerRandom);
-  function getComputerChoice() {
-    if (computerRandom <= 1) {
-      return "rock"
-    }
-    else if (computerRandom <= 2) {
-      return "scissors"
-    }
-    else {
-      return "paper"
-    }
-  };
-  const computerSelection = getComputerChoice();
-  console.log("The Machine played"+" "+computerSelection);
-  
-  const playerSelection = prompt("What do you choose between rock, paper and scissors?");
-  console.log("You played"+" "+playerSelection);
-
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-      return "Tie";
-    }
-    else if (playerSelection === "rock" && computerSelection === "scissors" 
-    || playerSelection === "scissors" && computerSelection === "paper" 
-    || playerSelection === "paper" && computerSelection === "rock") {
-      playerScore++;
-      return "Player won";
-    }
-    else {
-      computerScore++;
-      return "Machine won";
-    }
+  else if (computerRandom <= 2) {
+    return "scissors"
   }
+  else {
+    return "paper"
+  }
+};
+const computerSelection = getComputerChoice();
+console.log("The Machine played"+" "+computerSelection);
 
-  console.log(playRound(playerSelection, computerSelection));
+let playerScore = 0;
+let computerScore = 0;
 
-  console.log("Player score"+" "+ playerScore)
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Tie";
+  }
+  else if (playerSelection === "rock" && computerSelection === "scissors" 
+  || playerSelection === "scissors" && computerSelection === "paper" 
+  || playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++;
+    return "Player won";
+  }
+  else {
+    computerScore++;
+    return "Machine won";
+  }
+}
 
-  console.log("Machine score"+" "+ computerScore)
-  }
-  
-  if (playerScore > computerScore){
-    return "You win this game.";
-  }
-  else if (playerScore <  computerScore){
-     return "You lose this game"
-  }
-  else{
-    return "This game is a tie.";
-  }
-  }
-
-  console.log(game());
+function gameOver() {
+  return playerScore === 5 || computerScore === 5
+}
